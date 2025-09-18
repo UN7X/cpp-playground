82b899abc1d8083c5ac14e5fa83bf6edb3a9bcea
UN7X <unix@un7x.net>
2025-09-18 20:15:45 +0000

    i want to di- LIVE,  but NOT LIKE THIS

--- src/extra.cpp
+++ src/extra.cpp
@@ -3,47 +3,60 @@
 
 using namespace std;
 // "Severance" TV Show Trivia 
+
+// how do i create a list or dictionary of questions and answers?
+// idk
+// i can feel the inefficiency running through my veins
+// ghelp
+
 int main() {
-    cout << "Welcome to the Severance Trivia!" << endl << endl << "--------------------------------" << endl << endl;
-    // Int Questions
-    cout << "Int Questions --------------------" << endl << endl;
-    cout << "1. How many seasons of Severance are there as of 2025? (int)" << endl << "> " << endl << endl;
-    cout << "2. How many years after Season 1 did Season 2 come out? (int)" << endl << "> " << endl << endl;
-    cout << "3. What year did Season 1 of Severance come out? (int)" << endl << "> " << endl << endl;
-    cout << "4. What year did Season 2 of Severance come out? (int)" << endl << "> " << endl << endl;
+    cout << "Welcome to the Severance Trivia! \n\n-------------------------------- \n\n";
+    // Int Questions :3
+    cout << "Int Questions -------------------- \n\n";
+    cout << "1. How many seasons of Severance are there as of 2025? (int) \n\n> \n\n";
+    cout << "2. How many years after Season 1 did Season 2 come out? (int) \n\n> \n\n";
+    cout << "3. What year did Season 1 of Severance come out? (int) \n\n> \n\n";
+    cout << "4. What year did Season 2 of Severance come out? (int) \n\n> \n\n";
     // Float Questions
-    cout << "Float Questions ------------------" << endl << endl;
-    cout << "5. Using <season>.<episode number>, which episode was it revealed that Cobel grandfathered the Severance Procedure? (float)" << endl << "> " << endl << endl;
-    cout << "6. Using <season>.<episode number>, which episode was Helly introduced in? (float)" << endl << "> " << endl << endl;
-    cout << "7. Using <season>.<episode number>, which episode was Cobel fired in? (float)" << endl << "> " << endl << endl;
+    cout << "Float Questions ------------------ \n\n";
+    cout << "5. Using <season>.<episode number>, which episode was it revealed that Cobel grandfathered the Severance Procedure? (float) \n\n> \n\n";
+    cout << "6. Using <season>.<episode number>, which episode was Helly introduced in? (float) \n\n> \n\n";
+    cout << "7. Using <season>.<episode number>, which episode was Cobel fired in? (float) \n\n> \n\n";
     // String Questions
-    cout << "String Questions -----------------" << endl << endl;
-    cout << "8. What is the name of the company that created the severance procedure? (string)" << endl << "> " << endl << endl;
-    cout << "9. How did Gemma Scout die? (string)" << endl << "> " << endl << endl;
+    cout << "String Questions ----------------- \n\n";
+    cout << "8. What is the name of the company that created the severance procedure? (string) \n\n> \n\n";
+    cout << "9. How did Gemma Scout die? (string) \n\n> \n\n";
     // Char Questions
-    cout << "Character Questions --------------" << endl << endl;
-    cout << "10. What is Mr Eagan’s favorite breakfast? (char)" << endl
+    cout << "Character Questions -------------- \n\n";
+    cout << "10. What is Mr Eagan\'s favorite breakfast? (char)" << endl
          << "A] Pancake batter with Bacon bits" << endl
          << "B] Waffles and Cheese" << endl
          << "C] Eggs and Ham" << endl
-         << "D] 3 Raw eggs in milk." << endl
-         << "> " << endl << endl;
+         << "D] 3 Raw eggs in milk." << endl << endl
+         << "> \n\n";
     cout << "11. Which of these was NOT an episode title in both Season 1 and 2? (char)" << endl
          << "A] The Grim Barbarity of Optics and Design" << endl
          << "B] Sweet Alcohol" << endl
          << "C] The You You are" << endl
-         << "D] What’s for Dinner?" << endl
-         << "> " << endl << endl;
+         << "D] What\'s for Dinner?" << endl << endl
+         << "> \n\n";
     // Boolean Questions
-    cout << "Boolean Questions ----------------" << endl << endl;
-    cout << "12. True or False: The severance procedure is reversible? (boolean)" << endl << "> " << endl << endl;
-    cout << "13. True or False: In season 1, The break room is a place for relaxation and socialization? (boolean)" << endl << "> " << endl << endl;
-    cout << "14. True or False: Mark is the first person to undergo the severance procedure? (boolean)" << endl << "> " << endl << endl;
+    cout << "Boolean Questions ---------------- \n\n";
+    cout << "12. True or False: The severance procedure is reversible? (boolean) \n\n> \n\n";
+    cout << "13. True or False: In season 1, The break room is a place for relaxation and socialization? (boolean) \n\n> \n\n";
+    cout << "14. True or False: Mark is the first person to undergo the severance procedure? (boolean) \n\n> \n\n";
 
 
     return 0;
 }
 
+
+// bro python is easer like what is ts
+// i couldve just done a dictionary with questions and answers, wrote a loop to iterate through them and print them out, and another function to validate user input
+// but noooo i had to do it the hard way
+// im not even using c++17 features am i?
+// what even is c++17
+// ghelp 
 
 /* Questions and Answers: 
 How many seasons of Severance are there as of 2025?(int)
@@ -71,9 +84,9 @@ What is the name of the company that created the severance procedure?  (string)
 > Lumon Industries
 
 How did Gemma Scout die?(string)
-> She didn’t.
+> She didn\'t.
 
-What is Mr Eagan’s favorite breakfast? (char)
+What is Mr Eagan\'s favorite breakfast? (char)
 A) Pancake batter with Bacon bits
 B) Waffles and Cheese
 C) Eggs and Ham
@@ -85,7 +98,7 @@ Which of these was NOT an episode title in both Season 1 and 2?(char)
 A) The Grim Barbarity of Optics and Design
 B) Sweet Alcohol
 C) The You You are
-D) What’s for Dinner?
+D) What\'s for Dinner?
 
 > B
 
--- tests/main_test.cpp
+++ tests/main_test.cpp
@@ -2,8 +2,14 @@
 
 int add(int a, int b) { return a + b; }
 
+int subtract(int a, int b) { return a - b; }
+
 TEST_CASE("addition") {
   REQUIRE(add(2, 2) == 4);
+}
+
+TEST_CASE("subtraction") {
+  REQUIRE(subtract(4,2) == 2)
 }
 
 
